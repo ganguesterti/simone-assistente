aa2dfa52bbc3b99673a18b40aea49c417f53bc8a
ganguesterti <mateusnunes1552@gmail.com>
2025-04-23 14:47:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,12 @@ def webhook():
     print(f"[Simone] Respondendo para {numero}: {resposta}")
     return jsonify({"status": "ok", "resposta": resposta})
 
-if __name__ == "__main__":
-    app.run()
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 @app.route('/saúdez')
 def health_check():
